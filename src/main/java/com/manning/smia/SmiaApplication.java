6f417f6db20c8cab86d7f4b76b3b84ad4789b9d4
Mohit Kumar <mkumar@tekion.com>
2021-05-08 09:45:15 +0530

    Initial commit

---
+++ src/main/java/com/manning/smia/SmiaApplication.java
@@ -0,0 +1,13 @@
+package com.manning.smia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/manning/smia/SmiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manning.smia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
